8d1cc2dea618b256f8b15259b0e66c6db28aab59
zhouwei <zhou21192345@163.com>
2018-06-25 23:18:00 +0800

    项目重新打包上线

--- app.config.js
+++ app.config.js
@@ -3,6 +3,6 @@ module.exports = {
 		ak: 'U3zlCyHKIVtMa9UE56M5bjTo6I_eoZMK1ZjhQwKQ',
 		sk: 'SdbBSo_koVXKD_is9J-CHElrlGZ__UveGu5dJ042',
 		bucket: 'cnode',
-		host: 'http://pavsjosji.bkt.clouddn.com/'
+		host: 'https://pavsjosji.bkt.clouddn.com/'
 	}
 }
